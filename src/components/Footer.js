596298a9f12bb8525e488e85c298de50abcd4900
Yogesh <coolcoder9334@gmail.com>
2023-12-12 13:26:38 +0530

    updated the component code

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="bg-gray-900 items-center py-8">
+      <h1 className="text-sm text-center text-white">
+        Build with ❤️ by &nbsp;
+        <a
+          className="underline hover:text-blue-500"
+          href="https://github.com/yogeshkumarj7/Image-Rover"
+        >
+          Yogi
+        </a>
+        &nbsp; &copy; 2023
+      </h1>
+    </div>
+  );
+};
+export default Footer;
